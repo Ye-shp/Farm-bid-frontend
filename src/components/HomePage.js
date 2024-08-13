85cd2e7f83ac0dce402d0041a50ee51b3aa50aea
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-08-13 19:06:51 -0400

    Removed a few buttons that weren't responsive

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,10 +7,6 @@ const HomePage = () => (
     <header className="homepage-header">
       <h1>Welcome to Farm Bid Marketplace</h1>
       <p>Bid on fresh produce directly from local farmers.</p>
-      <div className="homepage-buttons">
-        <a href="/login" className="homepage-button">Login</a>
-        <a href="/register" className="homepage-button">Register</a>
-      </div>
     </header>
     <section className="homepage-info">
       <h2>Why Farm Bid?</h2>
