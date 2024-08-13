721b75f8030e6f84fbb40974a7c36e34a57c3a71
Yeabsira Alemu <yeabsiramesfin21@gmail.com>
2024-08-13 19:53:37 -0400

    Added backend api url

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://farm-bid-2dd5bc2872dd.herokuapp.com/';
 
 const api = axios.create({
   baseURL: API_URL,
